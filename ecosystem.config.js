bd2e68b77b01a36976b532fb0fb66c93c9591646
FileOnline Deployer <deploy@fileonline.local>
2026-02-19 01:54:09 +0800

    chore: add ecosystem.config.js for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+        {
+            name: 'fileonline',
+            script: 'npm',
+            args: 'start',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'production',
+                PORT: 3000
+            },
+        },
+    ],
+};
